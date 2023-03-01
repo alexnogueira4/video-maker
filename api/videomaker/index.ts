fda7c25c0e93475a5df58b33471799d3a000e0a8
Alexsander <alexsander@MacBook-Pro-de-Alexsander.local>
2023-03-01 19:59:29 -0300

    add files size to env file

--- api/videomaker/index.ts
+++ api/videomaker/index.ts
@@ -127,7 +127,7 @@ export default class VideoMaker {
 
   async sliceClips(clips = null) {
     clips = clips || await this.formatClips()
-    let arrayOfFilesSize = 20
+    let arrayOfFilesSize = process.env.ARRAY_OF_FILES_SIZE
     var clipsSliced: any = new Array(Math.ceil(clips.length / arrayOfFilesSize))
       .fill('')
       .map(_ => clips.splice(0, arrayOfFilesSize))
