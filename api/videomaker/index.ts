23562ae11f233195ca9cbb9b94372d817625f1f7
Alexsander <alexsander@MacBook-Pro-de-Alexsander.local>
2023-03-01 20:00:47 -0300

    adust files size type

--- api/videomaker/index.ts
+++ api/videomaker/index.ts
@@ -127,7 +127,7 @@ export default class VideoMaker {
 
   async sliceClips(clips = null) {
     clips = clips || await this.formatClips()
-    let arrayOfFilesSize = process.env.ARRAY_OF_FILES_SIZE
+    let arrayOfFilesSize: any = process.env.ARRAY_OF_FILES_SIZE || 20
     var clipsSliced: any = new Array(Math.ceil(clips.length / arrayOfFilesSize))
       .fill('')
       .map(_ => clips.splice(0, arrayOfFilesSize))
